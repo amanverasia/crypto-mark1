7d4b86c23bf9995f7d0deccdd878970b768c96cb
Aman Verasia <verasiaaman3@gmail.com>
2020-01-03 22:05:28 +0530

    Add files via upload

---
+++ encrypter.py
@@ -0,0 +1,31 @@
+#encrypting
+from random import randint
+text = input('Enter the text that you want to put: ')
+crypt = ''
+
+
+def random_n(n):
+  range_start = 10**(n-1)
+  range_end = (10**n)-1
+  return randint(range_start, range_end)
+
+def converter(text):
+	global crypt
+	for value in text:
+	  if ord(value)<100:
+	    crypt = crypt + '0' + str(ord(value))
+	  else:
+	    crypt = crypt + str(ord(value))
+	return crypt
+
+
+crypt = converter(text)
+print(f'This is the converted form {crypt} and it has a length of {len(crypt)}')
+
+num = random_n(len(crypt))
+pass1 = num*1 + int(crypt)
+pass2 = num*2 + int(crypt)
+print(f'The random seed was: {num}')
+if crypt[0]=='0':
+  print('\nIt has a 0 at the start\n')
+print(f'The two new keys are, \nPass1: {pass1} \nPass2: {pass2}')
