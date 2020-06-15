853dc7272213e162cd46f3731e00d78254fa2984
Aman Verasia <shabbysinger@protonmail.com>
2020-06-15 11:37:05 +0530

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -20,15 +20,15 @@ def converter(text):
 
 
 crypt = converter(text)
-print(f'This is the converted form {crypt} and it has a length of {len(crypt)}')
+print(f'\nThis is the converted form {crypt} and it has a length of {len(crypt)}\n\n')
 
 num = random_n(len(crypt))
 pass1 = num*1 + int(crypt)
 pass2 = num*2 + int(crypt)
-print(f'The random seed was: {num}')
+print(f'The random seed was: {num}\n')
 if crypt[0]=='0':
   print('\nIt has a 0 at the start\n')
-print(f'The two new keys are, \nPass1: {pass1} \nPass2: {pass2}')
+print(f'The two new keys are, \nPass1: {pass1} \n\nPass2: {pass2}')
 
 with open('pass.txt', 'w') as f:
   f.write('Pass1 is: ' + str(pass1))
