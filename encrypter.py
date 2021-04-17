6bfe5ffd186f03454db20aed988b4bc3e4869b9f
Aman Verasia <verasiaaman3@gmail.com>
2021-04-17 22:15:52 +0530

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,37 +1,40 @@
-#encrypting
 from random import randint
-text = input('Enter the text that you want to put: \n \n')
-crypt = ''
 
+plain_text = input('Orignal Text: ')
+x1 = 1
+x2 = 2
 
 def random_n(n):
   range_start = 10**(n-1)
   range_end = (10**n)-1
   return randint(range_start, range_end)
 
-def converter(text):
-	global crypt
+def coding_ascii(text):
+	crypt = ''
 	for value in text:
 	  if ord(value)<100:
 	    crypt = crypt + '0' + str(ord(value))
 	  else:
 	    crypt = crypt + str(ord(value))
 	return crypt
-
-
-crypt = converter(text)
-print(f'\nThis is the converted form {crypt} and it has a length of {len(crypt)}\n')
-
-num = random_n(len(crypt))
-pass1 = num*1 + int(crypt)
-pass2 = num*2 + int(crypt)
-print(f'The random seed was: {num}\n')
-if crypt[0]=='0':
-  print('\nIt has a 0 at the start\n')
-print(f'The two new keys are, \nPass1: {pass1} \n\nPass2: {pass2}')
-
-with open('pass.txt', 'w') as f:
-  f.write('Pass1 is: ' + str(pass1))
-  f.write('\n')
-  f.write('Pass2 is: ' + str(pass2))
-f.close()
+#coverting our values into numbers
+plain_text_ascii = coding_ascii(plain_text)
+#now starts the mathematical part
+slope = random_n(len(plain_text_ascii))
+#renaming slope as m and plain_text_ascii as c to make it better analogous to y = mx + c
+m = slope
+c = plain_text_ascii
+y1 = m*x1 + int(c)
+y2 = m*x2 + int(c)
+pass1 = y1
+pass2 = y2
+#printing time
+#print(f'Convereted ascii (c): {plain_text_ascii}\nLength: {len(plain_text_ascii)}')
+# print(' ')
+# print(f'Slope (m): {slope}')
+if c[0]=='0': print('There was a 0 at the beginning.')
+# print(' ')
+# print(f'x1: {x1}')
+# print(f'x2: {x2}')
+print(f'Pass1 (y1): {pass1}')
+print(f'Pass2 (y2): {pass2}')
