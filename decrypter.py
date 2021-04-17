a6de90143721b7540dc36ac271ac1280a8165c06
Aman Verasia <verasiaaman3@gmail.com>
2021-04-17 22:16:23 +0530

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,34 +1,36 @@
-#decrypting
-code1 = input('Enter the first code: ')
-code2 = input('Enter the second code: ')
-check = False
-while(check == False):
-  choice = input('Did the code have a zero at the beginning?(y/n): ')
-  if(choice == 'y' or choice == 'n'):
-    check = True
+pass1 = input('Pass1: ')
+pass2 = input('Pass2: ')
+x1 = 1
+x2 = 2
 
+def decoding_ascii(text):
+	plain_text = ''
+	for i in range(len(plain_text_ascii)):
+		if(i%3==0):
+			start = i
+			end = i + 3
+			plain_text = str(plain_text) + str(chr(int(plain_text_ascii[start:end])))
+	return plain_text
 
-slope = (int(code2)-int(code1))
-print(f'\nThe slope of it is: {str(slope)}\n')
-orignal = int(code1)-(int(slope))
+zero_at_start = input('Was there a zero at the start? (y/n): ')
 
-if(choice == 'y'):
-  orignal = '0' + str(orignal)
-if(choice == 'n'):
-	orignal = str(orignal)
+if(zero_at_start == 'y'):
+	zero_at_start = True
+else:
+	zero_at_start = False
 
-print(f'The converted orignal text was {orignal}\n')
-#print(len(str(pass1)))
+#converting all the variables into their mathematical form
+y1 = int(pass1)
+y2 = int(pass2)
+m = (y2-y1)
+c = int(y1 - m*x1)
+plain_text_ascii = str(c)
 
-va = ''
-for i in range(len(orignal)):
-  if(i%3==0):
-    start = i
-    end = i + 3
-    va = str(va) + str(chr(int(orignal[start:end])))
+if(zero_at_start):
+	plain_text_ascii = '0' + plain_text_ascii
 
-print('The orignal text was: "' + va +'"')
-
-with open('text.txt', 'w') as f:
-  f.write('Orignal text: ' + va)
-f.close()
+plain_text = decoding_ascii(plain_text_ascii)
+#printing time
+print(f'Slope (m): {m}')
+#print(f'Convereted ascii (c): {plain_text_ascii}\nLength: {len(plain_text_ascii)}')
+print(f'Orignal Text: {plain_text}')
